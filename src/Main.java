c08a8490c8fa789fbc331fce52606941a85ccaaf
Funkativity <victor.murta97@gmail.com>
2017-10-07 19:43:33 -0400

    Changed bump test stage

--- src/Main.java
+++ src/Main.java
@@ -74,21 +74,52 @@ public class Main {
 		
 		
 		
+		
 		//bump test
+		//move before 
 		mA.startSynchronization();
 		mB.forward();
 		mA.forward();
 		mA.endSynchronization();
+		
+		//stop when you hit a wall
 		float[] touchSample = new float[touch.sampleSize()];
 		while(touchSample[0]==0){
 			touch.fetchSample(touchSample, 0);
 		}
+		mA.startSynchronization();
+		mB.stop();
+		mA.stop();
+		mA.endSynchronization();
+		
+		//move back about 10cm, out of blanking distance
+		distanceToGo = .1f;
+		numRotations = ( distanceToGo / (RADIUS * 2 * PI));
+		angle = (int) (-360.0 * numRotations);
+		mA.startSynchronization();
+		mA.rotate(angle, false);
+		mB.rotate(angle, false);
+		mA.endSynchronization();
+		
+		//move back until sonar says you good
+		mA.startSynchronization();
+		mA.backward();
+		mB.backward();
+		mA.endSynchronization();
+		while(sonarSample[0] > distanceToWall){
+			sonic.fetchSample(sonarSample, 0);
+		}
+		mA.startSynchronization();
+		mB.stop();
+		mA.stop();
+		mA.endSynchronization();
 		
 		
 		
 		mA.close();
 		mB.close();
 	}
+	
 	
 
 }
