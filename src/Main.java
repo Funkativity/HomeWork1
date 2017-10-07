d8ec36b23a53b0a8cf03377f2e8ceff7c6b4d2e0
Armstrongthedolphin <1056608361@qq.com>
2017-10-07 17:01:20 -0400

    some change
    ???

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,8 @@ public class Main {
 		
 		RegulatedMotor mA = new EV3MediumRegulatedMotor(MotorPort.A);
 		RegulatedMotor mB = new EV3MediumRegulatedMotor(MotorPort.B);
-		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
-		EV3UltrasonicSensor ultrasensor = new EV3UltrasonicSensor(SensorPort.S2);
+		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S2);
+		EV3UltrasonicSensor ultrasensor = new EV3UltrasonicSensor(SensorPort.S4);
 		mA.synchronizeWith(new RegulatedMotor[] {mB});
 		
 		//TODO: Move Forward 150 cm, stop, and beep
